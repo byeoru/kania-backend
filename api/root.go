cbe57809fc06665df66688875aa4be8a74f6ded5
byeoru <byeoru215@gmail.com>
2024-12-13 01:35:25 +0900

    cors

--- api/root.go
+++ api/root.go
@@ -1,8 +1,14 @@
 package api
 
 import (
+	"log"
+
 	"github.com/byeoru/kania/service"
+	"github.com/byeoru/kania/types"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Api struct {
@@ -14,6 +20,19 @@ func NewApi(service *service.Service) *Api {
 	r := &Api{
 		engine:  gin.New(),
 		service: service,
+	}
+
+	// cors
+	r.engine.Use(cors.New(
+		cors.Config{
+			AllowOrigins: []string{"http://localhost:5173"},
+		}))
+
+	// politicalEntity validator
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("politicalEntity", types.ValidPoliticalEntity)
+	} else {
+		log.Fatal("validator setting error")
 	}
 
 	// router

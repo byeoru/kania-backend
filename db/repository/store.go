0138e21d8cf06f57d8660da0bcda955253e29889
byeoru <byeoru215@gmail.com>
2024-12-13 01:32:08 +0900

    add db transaction fn

--- db/repository/store.go
+++ db/repository/store.go
@@ -1,12 +1,15 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 )
 
 // Store provides all functions to execute db queries and transaction
 type Store interface {
 	Querier
+	ExecTx(ctx context.Context, fn func(*Queries) error) error
 }
 
 // SQLStore provides all functions to execute SQL queries and transactions
@@ -21,4 +24,23 @@ func NewStore(db *sql.DB) Store {
 		db:      db,
 		Queries: New(db),
 	}
+}
+
+// execTx executes a function within a database transaction
+func (store *SQLStore) ExecTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
 }
